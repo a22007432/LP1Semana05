4cebc410c4fe6c1ad23662690789d16a6dabf8a7
a22007432 <ssm.antonio@gmail.com>
2024-03-21 17:11:44 +0000

    finished ex 10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,11 +6,30 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Enemy loki = new Enemy("Loki");
+            /*Enemy loki = new Enemy("Loki");
             loki.SetName("   Grindalokki   ");
 
             Console.WriteLine
-            ($"Name is {loki.GetName()} and Hp is {loki.GetHealth()}");
+            ($"Name is {loki.GetName()} and Hp is {loki.GetHealth()}");*/
+
+            int enemy_n = int.Parse(args[0]);
+
+            Enemy[] enemy_array = new Enemy[enemy_n];
+
+            for(int i= 0; i < enemy_n; i ++)
+            {
+                Console.Write($"Nome do inimigo {i+1}: ");
+                string tmp_name = Console.ReadLine();
+                
+                Enemy rank_and_file = new Enemy(tmp_name);
+                enemy_array[i]= rank_and_file;
+            }
+
+            foreach(Enemy e in enemy_array)
+            {
+            Console.WriteLine($"{e.GetName()} {e.GetHealth()} {e.GetShield()}");
+            }
+
         }
     }
 }
