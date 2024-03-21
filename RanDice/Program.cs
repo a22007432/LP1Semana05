c292418ce7058585e7a11cfb916fb72d9ef912e8
a22007432 <ssm.antonio@gmail.com>
2024-03-21 16:30:30 +0000

    ex 5 dunzo

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+
+
 
 namespace RanDice
 {
@@ -6,7 +9,24 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            var rand = new Random(s);
+
+            int[] intarray = new int[n];
+
+            for(int i = 0; i < n; i ++)
+            {
+                int tmp = rand.Next(1,7);
+                Console.WriteLine(tmp);
+                intarray[i] = tmp;
+            }
+
+            int ret = intarray.Sum();
+            Console.WriteLine(ret);
+
+
         }
     }
 }
