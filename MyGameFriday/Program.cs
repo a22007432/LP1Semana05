ebd85c71ee93855c1ff47823e9963e85f62361a2
a22007432 <ssm.antonio@gmail.com>
2024-03-22 13:23:24 +0000

    added to sln

---
+++ MyGameFriday/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGameFriday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
